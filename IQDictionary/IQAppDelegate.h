bab344ce340cb38cb5a3b345b92d5687f3399299
BruceZCQ <brucezcq@gmail.com>
2013-10-26 22:18:50 +0800

    Initial Commit

---
+++ IQDictionary/IQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IQAppDelegate.h
+//  IQDictionary
+//
+//  Created by BruceZCQ on 13-10-26.
+//  Copyright (c) 2013年 OpeningO,Inc ( http://openingo.github.com ). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IQDictionary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IQDictionary
+//
+//  Created by BruceZCQ on 13-10-26.
+//  Copyright (c) 2013年 OpeningO,Inc ( http://openingo.github.com ). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IQAppDelegate class]));
+    }
+}
