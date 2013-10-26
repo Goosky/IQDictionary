194daa82ff588edff149fcb7965a9d13d74529be
BruceZCQ <brucezcq@gmail.com>
2013-10-26 22:22:25 +0800

    add sample dir

---
+++ IQDictionary/Sample/IQSample.h
@@ -0,0 +1,13 @@
+//
+//  IQSample.h
+//  IQDictionary
+//
+//  Created by BruceZCQ on 13-10-26.
+//  Copyright (c) 2013年 OpeningO,Inc ( http://openingo.github.com ). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IQSample : NSObject
+
+@end
---
+++ IQDictionary/Sample/IQSample.m
@@ -0,0 +1,13 @@
+//
+//  IQSample.m
+//  IQDictionary
+//
+//  Created by BruceZCQ on 13-10-26.
+//  Copyright (c) 2013年 OpeningO,Inc ( http://openingo.github.com ). All rights reserved.
+//
+
+#import "IQSample.h"
+
+@implementation IQSample
+
+@end
